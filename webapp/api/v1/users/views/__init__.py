8a9b2dede7cc318f0c8294bc4b62656042da0c07
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-10-26 19:21:34 +0900

    feat: username이 현재 중복되어 있는지 확인하는 API 추가

--- webapp/api/v1/users/views/__init__.py
+++ webapp/api/v1/users/views/__init__.py
@@ -1,5 +1,7 @@
+from .check_username_api_view import CheckUsernameAPIView
 from .email_update_api_view import EmailUpdateAPIView
 
 __all__ = [
     "EmailUpdateAPIView",
+    "CheckUsernameAPIView",
 ]
