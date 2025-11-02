fdae575922fce4f9af9309274b09f5f67039b4d9
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-11-02 16:17:20 +0900

    feat: 기본적인 child 정보를 다루는 serializer 추가

--- webapp/api/v1/users/serializers/__init__.py
+++ webapp/api/v1/users/serializers/__init__.py
@@ -1,3 +1,4 @@
+from .child_serializer import ChildSerializer
 from .email_update_serializer import EmailUpdateSerializer
 from .jwt_serializer import JWTSerializer
 from .login_serializer import LoginSerializer
@@ -5,6 +6,7 @@ from .register_serializer import RegisterSerializer
 from .user_detail_serializer import UserDetailSerializer
 
 __all__ = [
+    "ChildSerializer",
     "UserDetailSerializer",
     "JWTSerializer",
     "LoginSerializer",
