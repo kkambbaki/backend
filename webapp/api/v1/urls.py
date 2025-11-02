6b16832410d3d671aff6553e15139be09f611131
김보민 <157809900+bluebomin@users.noreply.github.com>
2025-11-02 21:13:07 +0900

    feat : Game1

--- webapp/api/v1/urls.py
+++ webapp/api/v1/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path("health-check/", include("api.v1.health_check.urls")),
     path("users/", include("api.v1.users.urls")),
+    path("games/", include("api.v1.games.urls")),
 ]
