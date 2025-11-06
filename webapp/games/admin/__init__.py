008353ba97ba9be9f4efe37d6ceb5fba9a7d2fd6
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-11-06 20:12:47 +0900

    feat: 게임 관련 모델에 대한 어드민 추가

---
+++ webapp/games/admin/__init__.py
@@ -0,0 +1,9 @@
+from .game_admin import GameAdmin
+from .game_result_admin import GameResultAdmin
+from .game_session_admin import GameSessionAdmin
+
+__all__ = [
+    "GameAdmin",
+    "GameSessionAdmin",
+    "GameResultAdmin",
+]
