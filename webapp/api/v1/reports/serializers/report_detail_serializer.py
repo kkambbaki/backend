00a3522cd2fa18aba8a0d707660ce8d59c40b768
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-11-10 02:12:30 +0900

    fix: TODO comment 삭제

--- webapp/api/v1/reports/serializers/report_detail_serializer.py
+++ webapp/api/v1/reports/serializers/report_detail_serializer.py
@@ -5,8 +5,6 @@ from api.v1.users.serializers import ChildSerializer
 
 from .game_report_detail_serializer import GameReportDetailSerializer
 
-# TODO: Report 정보가 제대로 GameResult의 meta에 따라 잘 만들어지는지 확인하고 내용 수정하기
-
 
 class ReportDetailSerializer(serializers.ModelSerializer):
     """리포트 상세 정보 Serializer"""
